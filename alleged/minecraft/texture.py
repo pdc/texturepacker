028f62c4f10e3927014068fd33e3962a3040ea9f
pdc <pdc@alleged.org.uk>
2011-01-13 23:37:49 +0000

    Add test for opening a source pack and getting a file from it

--- alleged/minecraft/texture.py
+++ alleged/minecraft/texture.py
@@ -14,19 +14,15 @@ from zipfile import ZipFile, ZIP_DEFLATED
 from StringIO import StringIO
 from base64 import b64decode
 
-class DocBase(object):
+class ResourceBase(object):
     def __init__(self, name):
         self.name = name
 
-    def get_content(self):
-        """Get the text for the resource (as a Unicode string)"""
-        raise NotImplemented('get_content')
-
     def get_bytes(self):
-        """Get the byte sequence that will go in the ZIP archive"""
-        return self.get_content().encode('UTF-8')
+        raise NotImplemented('get_bytes')
 
-class TextResource(DocBase):
+
+class TextResource(ResourceBase):
     """A document whose content is a literal string."""
     def __init__(self, name, content):
         super(TextResource, self).__init__(name)
@@ -34,6 +30,10 @@ class TextResource(DocBase):
 
     def get_content(self):
         return self.content
+
+    def get_bytes(self):
+        """Get the byte sequence that will go in the ZIP archive"""
+        return self.get_content().encode('UTF-8')
 
 class RecipeTexturePack(object):
     """Represents a texture pack."""
@@ -55,9 +55,63 @@ class RecipeTexturePack(object):
             for name, resource in sorted(self.resources.items()):
                 zip.writestr(name, resource.get_bytes())
 
+
+class SourcePack(object):
+    def __init__(self, zip_data):
+        self.zip = ZipFile(zip_data)
+        self.resources = {}
+
+    def __del__(self):
+        self.zip.close()
+
+    def get_resource(self, name):
+        res = self.resources.get(name)
+        if res:
+            return res
+        if name.endswith('.txt'):
+            text = self.zip.read(name).decode('UTF-8')
+            res = TextResource(name, text)
+        else:
+            res = SourceResource(self, name)
+        self.resources[name] = res
+        return res
+
+    def get_resource_bytes(self, name):
+        return self.zip.read(name)
+
+    @property
+    def label(self):
+        res = self.get_resource('pack.txt')
+        return res.get_content().split('\n', 1)[0]
+
+    @property
+    def desc(self):
+        res = self.get_resource('pack.txt')
+        return res.get_content().split('\n', 1)[1]
+
+class SourceResource(ResourceBase):
+    def __init__(self, source, name):
+        self.source = source
+        self.name = name
+        self.bytes = None
+
+    def get_bytes(self):
+        if self.bytes is None:
+            self.bytes = self.source.get_resource_bytes(self.name)
+        return self.bytes
+
+
 ###
 
-class TextResourceTests(unittest.TestCase):
+class TestCase(unittest.TestCase):
+    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'test_data'))
+
+    def get_resource(self, file_name):
+        with open(os.path.join(self.data_dir, file_name), 'rb') as strm:
+            bytes = strm.read()
+        return bytes
+
+class TextResourceTests(TestCase):
 	def setUp(self):
 		pass
 
@@ -67,7 +121,7 @@ class TextResourceTests(unittest.TestCase):
 	    self.assertEqual('Test pack\nBy Fréd the Deäd', doc.get_bytes())
 	    # This test relies on the encoding of the source file being UTF-8!!
 
-class RecipeTexturePackTests(unittest.TestCase):
+class RecipeTexturePackTests(TestCase):
     def test_pack_txt_from_init(self):
         pack = RecipeTexturePack(u'Test pack', u'It’s testy')
         self.assertEqual(u'Test pack\nIt’s testy', pack.get_resource('pack.txt').get_content())
@@ -85,16 +139,30 @@ class RecipeTexturePackTests(unittest.TestCase):
             self.assertEqual(u'Yummy pack\nIt’s tasty', zip.read('pack.txt').decode('UTF-8'))
             self.assertEqual(u'This is a news file.', zip.read('doc/news.txt').decode('UTF-8'))
 
-class SourceTexturePack(unittest.TestCase):
+class SourcePackTests(TestCase):
     def test_sign(self):
-        sign_data = b64decode('iVBORw0KGgoAAAANSUhEUgAAAIAAAABACAYAAADS1n9/AAABoElEQVR42u3bvUoDQRiF4eT2JBAEa0EwIGgRELTTgAG11cJqOzu1FERFtLCJSgpBcjfjLhidXRAJ5Mdhn8ALIU1mdt8cvsNmGiGERkzW74SUOdleTprq/Zg1DQL8Hw67BJAABJAAcxXgrL8WYrLEOemtlqjuLzVmL8DuSojJct6fr8Po5S4pijUXa6/+qqr7S43ZC9Dr/Hxh/j7LKS7o8PEyOb4TYCzAOAFSFWBRCZCsABJAAkiACQXob7ZDzHHOx+A2vD1cJMdRtx32tlolDrrl/aVG/mpGzKcFjF7vw/DpKjm0ADOAGcAMYAaQABJAAkiAGraAYu37G60SKTeArxYwh2cBkXFagBZgBjADmAG0AAlQowSIkABagBZQuxbgWYAWYAbQAswAWoAEkAA5xR8sU7v5g5tzCTCtFlCws76UFON1awFTPhiy6JMrIMCkNH/BDZcAIAD+roExVQFO889iXEQCuJAEAAFAABAABAABQAAQAAQAAUAAEAAEAAFAABAABAABQAAQAAQAAUAAEAAEAAGw2MOhIAAIAAKgFnwCP18LA1iM8R4AAAAASUVORK5CYII=')
+        im_data = self.get_resource('sign.png')
 
         # Create a ZIP file containing the sign.
+        strm = StringIO()
+        with ZipFile(strm, 'w') as zip:
+            zip.writestr('item/sign.png', im_data)
+            zip.writestr('pack.txt', 'Sign pack\nJust a test')
+        strm.seek(0)
 
         # Open it as a SourceTexturePack
+        pack = SourcePack(strm)
+        self.assertEqual('Sign pack', pack.label)
+        self.assertEqual('Just a test', pack.desc)
 
         # Check there is a SourceResource correspoding to the sign
+        res = pack.get_resource('item/sign.png')
+        self.assertEqual('item/sign.png', res.name)
 
         # Check the data returned from the resource maches what went in to the ZIP
+        self.assertEqual(im_data, res.get_bytes())
+
+
+
 if __name__ == '__main__':
 	unittest.main()
