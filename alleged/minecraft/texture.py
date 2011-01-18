56b0713df56eb09974965bb20a4e2959629bae0e
pdc <pdc@alleged.org.uk>
2011-01-18 08:41:52 +0000

    Add support for simplest possible recipe

--- alleged/minecraft/texture.py
+++ alleged/minecraft/texture.py
@@ -35,7 +35,7 @@ class TextResource(ResourceBase):
         """Get the byte sequence that will go in the ZIP archive"""
         return self.get_content().encode('UTF-8')
 
-class RecipeTexturePack(object):
+class RecipePack(object):
     """Represents a texture pack."""
     def __init__(self, label, desc):
         self.label = label
@@ -100,6 +100,22 @@ class SourceResource(ResourceBase):
             self.bytes = self.source.get_resource_bytes(self.name)
         return self.bytes
 
+class Mixer(object):
+    def __init__(self):
+        self.packs = {}
+
+    def add_pack(self, name, pack):
+        self.packs[name] = pack
+
+    def make(self, recipe):
+        new_pack = RecipePack(recipe['label'], recipe['desc'])
+        for ingredient in recipe['mix']:
+            pack = self.packs[ingredient['pack']]
+            for file_spec in ingredient['files']:
+                res_name = file_spec
+                res = pack.get_resource(res_name)
+                new_pack.add_resource(res)
+        return new_pack
 
 ###
 
@@ -110,6 +126,18 @@ class TestCase(unittest.TestCase):
         with open(os.path.join(self.data_dir, file_name), 'rb') as strm:
             bytes = strm.read()
         return bytes
+
+    def make_source_pack(self, name, desc, resources_by_file_name):
+        strm = StringIO()
+        with ZipFile(strm, 'w') as zip:
+            for file_name, res_name in resources_by_file_name.items():
+                zip.writestr(file_name, self.get_resource(res_name))
+            zip.writestr('pack.txt', '{0}\n{1}'.format(name, desc).encode('UTF-8'))
+        strm.seek(0)
+
+        # Open it as a SourceTexturePack
+        pack = SourcePack(strm)
+        return pack
 
 class TextResourceTests(TestCase):
 	def setUp(self):
@@ -123,17 +151,7 @@ class TextResourceTests(TestCase):
 
 class SourcePackTests(TestCase):
     def test_sign(self):
-        im_data = self.get_resource('sign.png')
-
-        # Create a ZIP file containing the sign.
-        strm = StringIO()
-        with ZipFile(strm, 'w') as zip:
-            zip.writestr('item/sign.png', im_data)
-            zip.writestr('pack.txt', 'Sign pack\nJust a test')
-        strm.seek(0)
-
-        # Open it as a SourceTexturePack
-        pack = SourcePack(strm)
+        pack = self.make_source_pack('Sign pack', 'Just a test', {'item/sign.png': 'sign.png'})
         self.assertEqual('Sign pack', pack.label)
         self.assertEqual('Just a test', pack.desc)
 
@@ -142,15 +160,15 @@ class SourcePackTests(TestCase):
         self.assertEqual('item/sign.png', res.name)
 
         # Check the data returned from the resource maches what went in to the ZIP
-        self.assertEqual(im_data, res.get_bytes())
+        self.assertEqual(self.get_resource('sign.png'), res.get_bytes())
 
-class RecipeTexturePackTests(TestCase):
+class RecipePackTests(TestCase):
     def test_pack_txt_from_init(self):
-        pack = RecipeTexturePack(u'Test pack', u'It’s testy')
+        pack = RecipePack(u'Test pack', u'It’s testy')
         self.assertEqual(u'Test pack\nIt’s testy', pack.get_resource('pack.txt').get_content())
 
     def test_zip_1(self):
-        pack = RecipeTexturePack(u'Yummy pack', u'It’s tasty')
+        pack = RecipePack(u'Yummy pack', u'It’s tasty')
         pack.add_resource(TextResource('doc/news.txt', 'This is a news file.'))
 
         # Now generate the archive and check it contains the expected files.
@@ -163,10 +181,10 @@ class RecipeTexturePackTests(TestCase):
             self.assertEqual(u'This is a news file.', zip.read('doc/news.txt').decode('UTF-8'))
 
     def test_zip_with_image_from_source_pack(self):
-        pack_ab = self.make_pack('a.png', 'b.png')
-        pack_c = self.make_pack('c.png')
+        pack_ab = self.make_source_pack('AB', 'Has A and B', {'a.png': 'a.png', 'b.png': 'b.png'})
+        pack_c = self.make_source_pack('C', 'Has C', {'c.png': 'c.png'})
 
-        new_pack = RecipeTexturePack(u'Composite pack', u'It’s composite')
+        new_pack = RecipePack(u'Composite pack', u'It’s composite')
         new_pack.add_resource(pack_ab.get_resource('a.png'))
         new_pack.add_resource(pack_c.get_resource('c.png'))
 
@@ -184,19 +202,43 @@ class RecipeTexturePackTests(TestCase):
             except KeyError:
                 pass
 
-    def make_pack(self, *file_names):
+class MixerTests(TestCase):
+    def test_b_plus_c(self):
+        mixer = Mixer()
+        mixer.add_pack('alpha_bravo', self.make_source_pack('AB', 'Has A and B', {'a.png': 'a.png', 'b.png': 'b.png'}))
+        mixer.add_pack('charlie', self.make_source_pack('C', 'Has C', {'c.png': 'c.png'}))
+
+        recipe = {
+            'label': 'Composite pack',
+            'desc': 'A crazy mixed-up pack',
+            'mix': [
+                {'pack': 'alpha_bravo', 'files': ['b.png']},
+                {'pack': 'charlie', 'files': ['c.png']}
+            ]
+        }
+        pack = mixer.make(recipe)
+
+        self.assertEqual('Composite pack', pack.label)
+        self.assertEqual('A crazy mixed-up pack', pack.desc)
+        self.check_pack(pack, {'b.png': 'b.png', 'c.png': 'c.png'}, ['a.png'])
+
+    def check_pack(self, pack, expected_contents, expected_absent):
         strm = StringIO()
-        with ZipFile(strm, 'w') as zip:
-            for file_name in file_names:
-                zip.writestr(file_name, self.get_resource(file_name))
-            zip.writestr('pack.txt', 'Sign pack\nJust a test')
+        pack.write_to(strm)
+
         strm.seek(0)
-
-        # Open it as a SourceTexturePack
-        pack = SourcePack(strm)
-
-        return pack
-
+        with ZipFile(strm, 'r') as zip:
+            for file_name, resource_name in expected_contents.items():
+                self.assertEqual(
+                        self.get_resource(resource_name),
+                        zip.read(file_name),
+                        'Expected to contain {0}'.format(file_name))
+            for file_name in expected_absent:
+                try:
+                    zip.read(file_name)
+                    self.fail('Should not find {0}'.format(file_name))
+                except KeyError:
+                    pass
 
 
 if __name__ == '__main__':
