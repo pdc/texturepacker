5193c5fdc5d9cf21da7805144ddb9f5640c37b0c
pdc <pdc@alleged.org.uk>
2011-01-30 14:15:03 +0000

    Add sample texture pack files

---
+++ script/frenden.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+test_texture.py
+
+Created by Damian Cugley on 2011-01-28.
+Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+dir_path = '/Users/pdc/Library/Application Support/minecraft/texturepacks'
+file_name = 'Frenden6_1a.zip'
+
+recipe = {
+    'name': 'Frenden (ALT)',
+    'desc': 'Frendan Graphic Pack with some alternate images used.'
+    'mix': {
+        'pack': 'frenden',
+        'files': [
+            {
+                'file': 'texture.png',
+                'source': 'texture.png',
+                'replace':
+            },
+        ]
+
+    }
+}
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
