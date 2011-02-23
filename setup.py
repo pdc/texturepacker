c8ddce832971f409034eb004c63b2ad5a1c370e6
pdc <pdc@alleged.org.uk>
2011-02-23 08:28:42 +0000

    Add Setuptools/Distribute support
    Now setup.py install will fetch and install dependencies.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 # encoding: UTF-8
 
-from distutils.core import setup
+from setuptools import setup
+
 setup(
     name='texturepacker',
-    version='0.1',
+    version='0.1.1',
     description='Assemble texture packs for Minecraft',
     long_description="""Provides program 'maketexture' for assembling texture packs for Minecraft
 using recipes written in a simple language expressed as JSON or as Python dicts,
@@ -22,17 +23,17 @@ added items.
     author_email='pdc@alleged.org.uk',
     url = 'http://pdc.github.com/texturepacker/',
 
-    requires=[
-        'PIL(>=1.1.7)',
-        'PyYAML(>=3.0.9)',
-        'httplib2(>=0.6.0)',
-
-        # Used for testing:
-        'mock(>=0.7.0b4)',
-        'nose(>=1.0.0)',
+    install_requires=[
+        'PIL>=1.1.7',
+        'PyYAML>=3.0.9',
+        'httplib2>=0.6.0',
+    ],
+    tests_require=[
+        'mock>=0.7.0b4',
+        'nose>=1.0.0',
     ],
 
-    packages=['alleged', 'alleged.minecraft'],
+    py_modules=['texturepacker'],
     scripts=['script/maketexture'],
     # The sample images & recipes are not included in a binary distribution.
 )
