537d7e3af77528635899141e8daed8430d7677e9
pdc <pdc@alleged.org.uk>
2011-02-23 20:08:07 +0000

    Add classifiers for PyPI categorization

--- setup.py
+++ setup.py
@@ -32,7 +32,17 @@ added items.
         'mock>=0.7.0b4',
         'nose>=1.0.0',
     ],
-
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Topic :: Games/Entertainment',
+        'Topic :: Utilities',
+    ],
     py_modules=['texturepacker'],
     scripts=['script/maketexture'],
     # The sample images & recipes are not included in a binary distribution.
