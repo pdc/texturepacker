6c2c0b8507dab05166e1261627c2c497f77581f7
pdc <pdc@alleged.org.uk>
2011-02-27 10:55:58 +0000

    Release 0.2

--- setup.py
+++ setup.py
@@ -2,23 +2,14 @@
 
 from setuptools import setup
 
+long_description = ''.join(list(open('README.txt'))[3:])
+print long_description
+
 setup(
     name='texturepacker',
-    version='0.1.1',
+    version='0.2',
     description='Assemble texture packs for Minecraft',
-    long_description="""Provides program 'maketexture' for assembling texture packs for Minecraft
-using recipes written in a simple language expressed as JSON or as Python dicts,
-and a library of Python classes that can be used to do the same thing in
-your own programs.
-
-Texture packs can be created by combining image files in PNG format,
-or images extracted from existing texture packs. This enables you to
-remix texture packs to use alternative textures for some elements
-if the original artist included them, or even to combine textures
-from two or more separate packs. One use for this might be to upgrade
-a pack designed for an earlier version of Minecraft to work with newly
-added items.
-""",
+    long_description=long_description,
     author='Damian Cugley',
     author_email='pdc@alleged.org.uk',
     url = 'http://pdc.github.com/texturepacker/',
