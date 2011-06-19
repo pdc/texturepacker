f3d810e484fdc0156ad6069268edd56045037c36
pdc <pdc@alleged.org.uk>
2011-06-19 10:08:35 +0100

    Bump version to 0.8.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = ''.join(list(open('README.txt'))[3:])
 
 setup(
     name='texturepacker',
-    version='0.8.3',
+    version='0.8.4',
     description='Assemble texture packs for Minecraft',
     long_description=long_description,
     author='Damian Cugley',
